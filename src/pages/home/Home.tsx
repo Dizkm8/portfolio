149b3ff33b7334177896f1b69a43e93c218e9191
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2024-02-27 23:38:32 -0300

    fix: move SkillsView import

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import { Divider } from '@mui/material';
 import AboutMeView from './about-me/AboutMeView';
-import SkillsView from './skills/SkillsView';
 import ProjectsView from './projects/ProjectsView';
+import SkillsView from './skills/SkillsView';
 
 const Home = () => {
     return (
