4be143c994b21df35c44fb3ef7e702456bb6d21a
DAVID ARAYA CÁDIZ <david.araya@alumnos.ucn.cl>
2024-03-01 18:57:52 -0300

    fix: skills removed from view

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
             <Divider />
             <ProjectsView />
             <Divider />
-            <SkillsView />
+            {/* <SkillsView /> */}
 
         </>
     );
